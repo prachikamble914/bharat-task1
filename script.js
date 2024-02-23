eb1849d99806c1538bb7103b273ed5a50f6c5339
Prachi Kamble <146330849+prachikamble914@users.noreply.github.com>
2024-02-23 22:14:21 +0530

    Add files via upload
    Portfolio website

---
+++ script.js
@@ -0,0 +1,5 @@
+// console.log("script running")
+// document.querySelector('.cross').Style.display = 'none';
+// document.querySelector('.ham').addEventListener("click", ()=>{
+//    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+// })
